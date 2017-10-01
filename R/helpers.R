1c128b7a895281585888bfc79feaa357df1dcbe5
Jeff Hughes <jeff.hughes@gmail.com>
2017-10-01 14:37:02 -0400

    Fixes documentation for several functions.

--- R/helpers.R
+++ R/helpers.R
@@ -1,115 +1,75 @@
 #' Return results of a parameter test.
 #'
-#' \code{results} is a generic function that extracts the raw data from a
-#' parameter test.
+#' \code{results} returns the raw data from a parameter test.
 #'
-#' @param test A paramtest object.
-#' @param ... Additional arguments to be passed to the particular method for the
-#'   given object tye.
-#' @return The form of the value returned by \code{results} depends on the class
-#'   of its argument. See the documentation of the particular methods for
-#'   details of what is produced by that method.
-#' @seealso \code{\link{results.paramtest}}
+#' @param test An object of type 'paramtest'.
+#' @param ... Not currently implemented; used to ensure consistency with S3 generic.
+#' @return Returns a data frame with all the data returned from each test.
 #' @export
 results <- function(test, ...) UseMethod('results')
 
 
-#' Return results of a parameter test.
-#'
-#' \code{results.paramtest} returns the raw data from a parameter test.
-#'
-#' @param test An object of type 'paramtest'.
-#' @return Returns a data frame with all the data returned from each test.
+#' @describeIn results
 #' @export
-results.paramtest <- function(test) {
+results.paramtest <- function(test, ...) {
     return(test$results)
 }
 
 
 #' Return the parameter values that were tested by paramtest.
 #'
-#' \code{tests} is a generic function that extracts information about the set of
-#' specific tests (parameter values) for a parameter test.
+#' \code{tests} extracts information about the set of specific tests (parameter
+#' values) for a parameter test.
 #'
-#' @param test A paramtest object.
-#' @param ... Additional arguments to be passed to the particular method for the
-#'   given object tye.
-#' @return The form of the value returned by \code{tests} depends on the class
-#'   of its argument. See the documentation of the particular methods for
-#'   details of what is produced by that method.
-#' @seealso \code{\link{tests.paramtest}}
+#' @param test An object of type 'paramtest'.
+#' @param ... Not currently implemented; used to ensure consistency with S3 generic.
+#' @return Returns a data frame with one row for each set of tests that
+#'   was performed.
 #' @export
 tests <- function(sim, ...) UseMethod('tests')
 
 
-#' Return the parameter values that were tested by paramtest.
-#'
-#' \code{tests.paramtest} extracts information about the set of specific tests
-#' (parameter values) for a parameter test.
-#'
-#' @param test An object of type 'paramtest'.
-#' @return Returns a data frame with one row for each set of tests that
-#'   was performed.
+#' @describeIn tests
 #' @export
-tests.paramtest <- function(test) {
+tests.paramtest <- function(test, ...) {
     return(test$tests)
 }
 
 
 #' Return the number of iterations performed by a parameter test.
 #'
-#' \code{n.iter} is a generic function that extracts information about the
-#' number of iterations (per specific test) performed by a parameter test.
+#' \code{n.iter} extracts information about the number of iterations (per
+#' specific test) performed by a parameter test.
 #'
-#' @param test A paramtest object.
-#' @param ... Additional arguments to be passed to the particular method for the
-#'   given object tye.
-#' @return The form of the value returned by \code{n.iter} depends on the class
-#'   of its argument. See the documentation of the particular methods for
-#'   details of what is produced by that method.
-#' @seealso \code{\link{n.iter.paramtest}}
+#' @param test An object of type 'paramtest'.
+#' @param ... Not currently implemented; used to ensure consistency with S3 generic.
+#' @return Returns the number of iterations done in each test.
 #' @export
 n.iter <- function(test, ...) UseMethod('n.iter')
 
 
-#' Return the number of iterations performed by a parameter test.
-#'
-#' \code{n.iter.paramtest} extracts information about the number of iterations
-#' (per specific test) performed by a parameter test.
-#'
-#' @param test An object of type 'paramtest'.
-#' @return Returns the number of iterations done in each test.
+#' @describeIn n.iter
 #' @export
-n.iter.paramtest <- function(test) {
+n.iter.paramtest <- function(test, ...) {
     return(test$n.iter)
 }
 
 
 #' Return the timing information of a parameter test.
 #'
-#' \code{timing} is a generic function that extracts the raw data from a
-#' parameter test.
+#' \code{timing} returns the information about how long a parameter test took.
 #'
-#' @param test A paramtest object.
-#' @param ... Additional arguments to be passed to the particular method for the
-#'   given object tye.
-#' @return The form of the value returned by \code{timing} depends on the class
-#'   of its argument. See the documentation of the particular methods for
-#'   details of what is produced by that method.
-#' @seealso \code{\link{timing.paramtest}}
+#' @param test An object of type 'paramtest'.
+#' @param ... Not currently implemented; used to ensure consistency with S3 generic.
+#' @return Returns an object of class "proc_time" with information about how
+#'   long the parameter test process took.
 #' @export
 timing <- function(test, ...) UseMethod('timing')
 
 
-#' Return the timing information of a parameter test.
-#'
-#' \code{timing.paramtest} returns the raw data from a parameter test.
-#'
-#' @param test An object of type 'paramtest'.
-#' @return Returns an object of class "proc_time" with information about how
-#'   long the parameter test process took.
+#' @describeIn timing
 #' @export
-timing.paramtest <- function(test) {
+timing.paramtest <- function(test, ...) {
     return(test$timing)
 }
 
